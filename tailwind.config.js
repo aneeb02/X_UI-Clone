d47f97df05e371be714a6d405c32e1e13890ac05
bharajuice <aneeb.02@gmail.com>
2024-07-25 22:45:36 +0500

    created sidebar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'dm-sans': ['DM Sans', 'sans-serif'],
+        'jost': ['Jost', 'sans-serif'],
+        'nunito-sans': ['Nunito Sans', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
